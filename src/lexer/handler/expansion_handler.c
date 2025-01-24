5415420824592db0abe5ed877a7e83740bfcdf47
seungahpyun <spyun@student.codam.nl>
2025-01-24 09:05:06 +0100

    adding rule for quote

--- src/lexer/handler/expansion_handler.c
+++ src/lexer/handler/expansion_handler.c
@@ -6,14 +6,16 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/01/20 10:40:42 by spyun         #+#    #+#                 */
-/*   Updated: 2025/01/23 17:27:33 by spyun         ########   odam.nl         */
+/*   Updated: 2025/01/24 09:04:12 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 
-/* Check environment variable characters
-   (alphabets, numbers, _ starting with $) */
+/*
+** Check environment variable characters
+** (alphabets, numbers, _ starting with $)
+*/
 static int	is_valid_var_char(char c)
 {
 	return (ft_isalnum(c) || c == '_');
@@ -35,11 +37,20 @@ static char	*handle_special_param(char *str, int *pos, t_quote_state state)
 ** Extract environment variable names and return their values
 ** Returns an empty string for non-existent variables
 */
-static char	*expand_var_name(char *str, int start, int len)
+static char	*handle_simple_expansion(char *str, int *pos, t_quote_state state)
 {
+	int		start;
+	int		len;
 	char	*var_name;
 	char	*value;
 
+	start = *pos + 1;
+	len = 0;
+	while (str[start + len] && is_valid_var_char(str[start + len]))
+		len++;
+	if (len == 0)
+		return (ft_strdup("$"));
+	*pos = start + len - 1;
 	var_name = ft_substr(str, start, len);
 	if (!var_name)
 		return (NULL);
@@ -50,26 +61,20 @@ static char	*expand_var_name(char *str, int start, int len)
 	return (ft_strdup(value));
 }
 
-/* Handles environment variable expansion.
-   It is responsible for expanding variables
-   that start with $ to their actual value. */
+/*
+** Handles environment variable expansion.
+** It is responsible for expanding variables
+** that start with $ to their actual value. */
 char	*get_var_value(char *str, int *pos, t_quote_state state)
 {
-	char	*value;
-	int		start;
-	int		len;
+	char	*special_value;
 
 	if (state.quote_char == '\'')
 		return (ft_strdup("$"));
-	value = handle_special_param(str, pos, state);
-	if (value)
-		return (value);
-	start = *pos + 1;
-	len = 0;
-	while (str[start + len] && is_valid_var_char(str[start + len]))
-		len++;
-	if (len == 0)
-		return (ft_strdup("$"));
-	*pos = start + len - 1;
-	return (expand_var_name(str, start, len));
+	special_value = handle_special_param(str, pos, state);
+	if (special_value)
+		return (special_value);
+	if (str[*pos + 1] == '{')
+		return (handle_braced_expansion(str, pos, state));
+	return (handle_simple_expansion(str, pos, state));
 }
