b4c6616a501f3ddc6c125db093a45784976828d4
seungahpyun <spyun@student.codam.nl>
2025-01-28 14:47:00 +0100

    change parser struct name

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/01/20 13:46:08 by spyun         #+#    #+#                 */
-/*   Updated: 2025/01/27 10:59:06 by spyun         ########   odam.nl         */
+/*   Updated: 2025/01/28 14:46:42 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ typedef struct s_ast_node
 	struct s_ast_node	*right;
 }	t_ast_node;
 
-typedef enum e_valid_error
+typedef enum e_cmd_valid_error
 {
 	VALID_SUCCESS,
 	VALID_EMPTY_CMD,
@@ -41,27 +41,27 @@ typedef enum e_valid_error
 	VALID_MISSING_FILENAME,
 	VALID_SYNTAX_ERROR,
 	VALID_TOO_MANY_ARGS,
-}	t_valid_error;
+}	t_cmd_valid_error;
 
-t_ast_node		*parse(t_token *tokens);
-void			free_ast(t_ast_node *node);
-t_ast_node		*create_ast_node(t_token_type type);
-void			add_arg_to_node(t_ast_node *node, char *arg);
-t_ast_node		*parse_pipeline(t_token **token);
+t_ast_node			*parse(t_token *tokens);
+void				free_ast(t_ast_node *node);
+t_ast_node			*create_ast_node(t_token_type type);
+void				add_arg_to_node(t_ast_node *node, char *arg);
+t_ast_node			*parse_pipeline(t_token **token);
 
-t_ast_node		*parse_command(t_token **token);
-t_ast_node		*create_logic_node(t_token **token);
-t_ast_node		*handle_logic_error(void);
-t_ast_node		*process_logic_operator(t_token **token,
+t_ast_node			*parse_command(t_token **token);
+t_ast_node			*create_logic_node(t_token **token);
+t_ast_node			*handle_logic_error(void);
+t_ast_node			*process_logic_operator(t_token **token,
 					t_ast_node *left, t_ast_node *logic_node);
-t_ast_node		*handle_logic_sequence(t_token **token, t_ast_node *left);
-t_ast_node		*parse_logic(t_token **token);
-t_ast_node		*parse_complete_bonus(t_token **token);
-t_ast_node		*parse_group(t_token **token);
-t_ast_node		*parse_redirection(t_token **token);
+t_ast_node			*handle_logic_sequence(t_token **token, t_ast_node *left);
+t_ast_node			*parse_logic(t_token **token);
+t_ast_node			*parse_complete_bonus(t_token **token);
+t_ast_node			*parse_group(t_token **token);
+t_ast_node			*parse_redirection(t_token **token);
 
-t_valid_error	validate_command_syntax(t_ast_node *node);
-t_valid_error	validate_redirection_syntax(t_redirection *redirs);
-char			*get_validation_error_msg(t_valid_error error);
-int				is_valid_command_name(const char *cmd);
+t_cmd_valid_error	validate_command_syntax(t_ast_node *node);
+t_cmd_valid_error	validate_redirection_syntax(t_redirection *redirs);
+char				*get_validation_error_msg(t_cmd_valid_error error);
+int					is_valid_command_name(const char *cmd);
 #endif
