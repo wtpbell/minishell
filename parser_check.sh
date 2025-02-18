09213366e77efb2e7357ac1d188bb67fe3a2fbce
seungahpyun <spyun@student.codam.nl>
2025-02-18 11:46:59 +0100

    adding more cases

--- parser_check.sh
+++ parser_check.sh
@@ -149,3 +149,12 @@ echo "Success rate: $(( (PASSED * 100) / TOTAL ))%"
 # Memory leak check reminder
 echo -e "\n${BLUE}Don't forget to check for memory leaks:${NC}"
 echo "Run: valgrind --leak-check=full ./minishell"
+
+echo "\$PATH"'$PATH'
+echo "${HOME}${PWD}"
+echo "'"'
+echo '"'"
+echo "$?"'$?'
+echo "$$"'$$'
+echo "$0"'$0'
+echo 'hello\'world'
