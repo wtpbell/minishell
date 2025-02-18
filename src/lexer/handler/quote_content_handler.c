63c3467347fae99899077c1bb376ca47724f2731
seungahpyun <spyun@student.codam.nl>
2025-02-18 16:36:24 +0100

    better memery handle

--- src/lexer/handler/quote_content_handler.c
+++ src/lexer/handler/quote_content_handler.c
@@ -6,7 +6,7 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/02/18 15:19:39 by spyun         #+#    #+#                 */
-/*   Updated: 2025/02/18 15:24:32 by spyun         ########   odam.nl         */
+/*   Updated: 2025/02/18 16:36:00 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,14 @@ static char	*handle_escaped_quote(t_tokenizer *tokenizer,
 
 	content = ft_substr(tokenizer->input, *start,
 			tokenizer->position - *start);
+	if (!content)
+		return (NULL);
 	result = ft_strjoin_free(result, content);
+	if (!result)
+		return (NULL);
 	result = ft_strjoin_char(result, quote);
+	if (!result)
+		return (NULL);
 	tokenizer->position += 2;
 	*start = tokenizer->position;
 	return (result);
@@ -33,6 +39,8 @@ char	*extract_quote_content(t_tokenizer *tokenizer,
 	char	*result;
 
 	result = ft_strdup("");
+	if (!result)
+		return (NULL);
 	while (tokenizer->input[tokenizer->position]
 		&& tokenizer->input[tokenizer->position] != quote)
 	{
