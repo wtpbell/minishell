c7818e78611dc7515ef9045f2b20d610c6f1fa49
seungahpyun <spyun@student.codam.nl>
2025-02-20 09:22:28 +0100

    $PATH is working but not with $?

--- src/lexer/handler/quote_expansion_handler.c
+++ src/lexer/handler/quote_expansion_handler.c
@@ -6,46 +6,64 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/02/18 15:24:00 by spyun         #+#    #+#                 */
-/*   Updated: 2025/02/20 08:40:34 by spyun         ########   odam.nl         */
+/*   Updated: 2025/02/20 09:21:10 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 
-static char *handle_double_quote_expansion(t_tokenizer *tokenizer, char *result)
+static char	*remove_backslash(char *str)
 {
-    char    *expanded;
-    char    *temp;
-    int     i;
-    int     j;
+	int		i;
+	int		j;
+	char	*result;
 
-    temp = ft_strdup("");
-    i = 0;
-    while (result[i])
-    {
-        if (result[i] == '\\' && result[i + 1] == '$')
-        {
-            temp = ft_strjoin_char(temp, '$');
-            i += 2;
-        }
-        else
-            temp = ft_strjoin_char(temp, result[i++]);
-    }
-    free(result);
-    result = temp;
+	if (!str || !ft_strchr(str, '\\'))
+		return (str);
+	result = ft_strdup(str);
+	if (!result)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (str[i] == '\\' && str[i + 1])
+			i++;
+		result[j++] = str[i++];
+	}
+	result[j] = '\0';
+	free(str);
+	return (result);
+}
 
-    if (ft_strcmp(result, "$?") == 0)
-    {
-        free(result);
-        result = ft_itoa(g_exit_status);
-    }
-    else if (ft_strchr(result, '$'))
-    {
-        expanded = handle_expansion(tokenizer, result);
-        free(result);
-        result = expanded;
-    }
-    return (result);
+static char	*handle_double_quote_expansion(t_tokenizer *tokenizer,
+	char *result)
+{
+	char	*expanded;
+
+	if (!result || !tokenizer)
+		return (NULL);
+	if (ft_strchr(result, '\\'))
+	{
+		if (ft_strncmp(result, "\\$?", 3) == 0)
+			return (free(result), ft_strdup("$?"));
+		result = remove_backslash(result);
+		if (!result)
+			return (NULL);
+		return (result);
+	}
+	if (ft_strncmp(result, "$?", 2) == 0)
+	{
+		free(result);
+		return (ft_itoa(g_exit_status));
+	}
+	if (ft_strchr(result, '$'))
+	{
+		expanded = handle_expansion(tokenizer, result);
+		free(result);
+		return (expanded);
+	}
+	return (result);
 }
 
 char	*extract_quoted_content_with_expansion(t_tokenizer *tokenizer,
