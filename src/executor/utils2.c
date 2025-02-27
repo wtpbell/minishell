6b1f4f2d1335b5a749126262cae86b277e651127
Bell Wong <bewong@f1r6s17.codam.nl>
2025-02-27 10:34:22 +0100

    change the exit status

--- src/executor/utils2.c
+++ src/executor/utils2.c
@@ -6,7 +6,7 @@
 /*   By: bewong <bewong@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/01/31 16:48:58 by bewong        #+#    #+#                 */
-/*   Updated: 2025/02/23 21:57:51 by bewong        ########   odam.nl         */
+/*   Updated: 2025/02/27 10:31:38 by bewong        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,7 @@ static int	resolve_command(t_ast_node *node)
 	{
 		error(node->args[0], "Command not found");
 		set_underscore(node->argc, node->args);
-		return (set_exit_status(127), 127);
+		return (set_exit_status(0), 0);
 	}
 	free(node->args[0]);
 	node->args[0] = tmp;
