d751aab5e2f426fcdecbdef0378118637e71308c
seungahpyun <spyun@student.codam.nl>
2025-03-05 09:50:38 +0100

    norm fix

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/01/20 13:46:08 by spyun         #+#    #+#                 */
-/*   Updated: 2025/03/04 10:12:56 by spyun         ########   odam.nl         */
+/*   Updated: 2025/03/05 09:36:00 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ typedef struct s_redir
 	t_token_type			type;
 	char					*file;
 	char					*delimiter;
-    char					*heredoc_file;
+	char					*heredoc_file;
 	int						fd;
 	int						flags;
 	t_quote_type			quote_type;
