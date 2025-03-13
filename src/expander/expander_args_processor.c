6c7aa8306788ccd86b57aaa2fdcc159568d2a4cd
seungahpyun <spyun@student.codam.nl>
2025-03-13 11:11:39 +0100

    following norm

--- src/expander/expander_args_processor.c
+++ src/expander/expander_args_processor.c
@@ -6,48 +6,13 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/02/26 14:27:42 by spyun         #+#    #+#                 */
-/*   Updated: 2025/03/13 10:59:18 by spyun         ########   odam.nl         */
+/*   Updated: 2025/03/13 11:10:49 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "parser.h"
 #include "expander.h"
-
-static void	handle_exit_status_expansion(t_ast_node *node, int i)
-{
-	char	*status;
-	char	*result;
-
-	status = ft_strdup(get_env_value(*get_env_list(), "?"));
-	if (!status)
-		status = ft_strdup("0");
-	if (node->args[i][2] != '\0')
-	{
-		result = ft_strjoin(status, node->args[i] + 2);
-		free(status);
-		free(node->args[i]);
-		node->args[i] = result;
-	}
-	else
-	{
-		free(node->args[i]);
-		node->args[i] = status;
-	}
-}
-
-static char	*extract_var_name(char *arg, int *var_len)
-{
-	char	*var_name;
-	int		len;
-
-	len = 0;
-	while (arg[len] && (ft_isalnum(arg[len]) || arg[len] == '_'))
-		len++;
-	var_name = ft_substr(arg, 0, len);
-	*var_len = len;
-	return (var_name);
-}
 
 static void	handle_regular_env_var(t_ast_node *node, t_env **env_list, int i)
 {
@@ -57,7 +22,7 @@ static void	handle_regular_env_var(t_ast_node *node, t_env **env_list, int i)
 	char	*rest_of_arg;
 	int		var_len;
 
-	var_name = extract_var_name(node->args[i] + 1, &var_len);
+	var_name = extract_var_name_expander(node->args[i] + 1, &var_len);
 	rest_of_arg = ft_strdup(node->args[i] + 1 + var_len);
 	env_value = get_env_value(*env_list, var_name);
 	if (env_value)
