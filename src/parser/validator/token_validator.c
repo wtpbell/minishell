491a3caa86dace2ca4608ec7d8febd3020589702
seungahpyun <spyun@student.codam.nl>
2025-03-14 14:20:27 +0100

    subshell syntax

--- src/parser/validator/token_validator.c
+++ src/parser/validator/token_validator.c
@@ -6,12 +6,55 @@
 /*   By: spyun <marvin@42.fr>                         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/02/28 09:59:36 by spyun         #+#    #+#                 */
-/*   Updated: 2025/03/13 09:49:19 by bewong        ########   odam.nl         */
+/*   Updated: 2025/03/14 14:18:54 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 #include "executor.h"
+
+static void	print_paren_error(void)
+{
+	ft_putstr_fd(RED, STDERR_FILENO);
+	ft_putendl_fd("minishell: syntax error: unmatched parentheses",
+		STDERR_FILENO);
+	ft_putstr_fd(RESET, STDERR_FILENO);
+}
+
+static void	print_token_error(char *content)
+{
+	ft_putstr_fd(RED, STDERR_FILENO);
+	ft_putstr_fd("minishell: syntax error near unexpected token `",
+		STDERR_FILENO);
+	ft_putstr_fd(content, STDERR_FILENO);
+	ft_putendl_fd("'", STDERR_FILENO);
+	ft_putstr_fd(RESET, STDERR_FILENO);
+	set_exit_status(2);
+}
+
+static int	check_consecutive_paren(t_token *tokens)
+{
+	t_token	*curr;
+	int		last_was_rparen;
+
+	curr = tokens;
+	last_was_rparen = 0;
+	while (curr)
+	{
+		if (last_was_rparen && curr->type == TOKEN_LPAREN)
+		{
+			print_token_error(curr->content);
+			return (0);
+		}
+		if (curr->type == TOKEN_RPAREN)
+			last_was_rparen = 1;
+		else if (curr->type == TOKEN_AND || curr->type == TOKEN_OR
+			|| curr->type == TOKEN_PIPE)
+			last_was_rparen = 0;
+		curr = curr->next;
+	}
+	return (1);
+}
 
 int	is_valid_token_sequence(t_token *tokens)
 {
@@ -19,13 +62,10 @@ int	is_valid_token_sequence(t_token *tokens)
 		return (0);
 	if (!validate_parentheses(tokens))
 	{
-		ft_putstr_fd(RED, STDERR_FILENO);
-		ft_putendl_fd("minishell: syntax error: unmatched parentheses",
-			STDERR_FILENO);
-		ft_putstr_fd(RESET, STDERR_FILENO);
+		print_paren_error();
 		return (0);
 	}
-	return (1);
+	return (check_consecutive_paren(tokens));
 }
 
 static void	print_syntax_error(char *token_str)
